5db74db8f30b77b764c4ce57f83d386045252a0e
shawon-MG <work.shawomg@gmail.com>
2022-05-25 09:36:35 +0600

    Navbar added, Routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Contact from './Pages/Contact/Contact';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Banner from './Pages/Home/Banner';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Header from './Pages/Shared/Header';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1> Final Project Initialization </h1>
+      <Header />
+      <Routes>
+
+        <Route path='/' element={<Banner />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+
+      </Routes>
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+
+  daisyui: {
+    themes: [
+      {
+        finalProjectTheme: {
+          primary: "#fbbf24",
+          secondary: "#fb923c",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+
+  plugins: [require("daisyui")],
+}
