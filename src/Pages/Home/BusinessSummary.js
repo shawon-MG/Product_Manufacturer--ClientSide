952020c9e45039a42467cc04dfa291abdb0c1446
shawon-MG <work.shawomg@gmail.com>
2022-05-25 19:15:18 +0600

    Business summary added using stat

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -3,7 +3,28 @@ import React from 'react';
 const BusinessSummary = () => {
     return (
         <div>
-            <h1> Business Summary</h1>
+            <h1 className='text-center text-2xl font-bold mt-16'> Our Business Summary at a Glance </h1>
+            <div class="stats stats-vertical lg:stats-horizontal flex shadow-lg">
+
+                <div class="stat justify-center">
+                    <div class="stat-title"> Regular Customers </div>
+                    <div class="stat-value">31K</div>
+                    <div class="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+
+                <div class="stat">
+                    <div class="stat-title"> Revenue  </div>
+                    <div class="stat-value">4,20,00,000</div>
+                    <div class="stat-desc"> ↗︎ 400 (22%) </div>
+                </div>
+
+                <div class="stat">
+                    <div class="stat-title"> Collaborations with other businesses </div>
+                    <div class="stat-value"> 480 </div>
+                    <div class="stat-desc"> ↗︎ 90 (14%)</div>
+                </div>
+
+            </div>
         </div>
     );
 };
